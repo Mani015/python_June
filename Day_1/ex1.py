f8419c65e1bb60c250a68d1bde00425f39978829
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-06-23 17:08:41 +0530

    'py'

---
+++ Day_1/ex1.py
@@ -0,0 +1,58 @@
+
+# Python is an interpreted and general-purpose programming language that emphasizes code readability with its use of significant indentation.
+# Python is a general-purpose, high-level, interpreted, object-oriented programming language used for various applications.
+
+
+
+# Python was developed by Guido Van Rossum in 1989 while working at National Research Institute in the Netherlands.
+# But officially, Python was made available to the public in 1991.
+
+
+# Python code syntax uses English keywords, making it easy to understand.
+# Therefore, Python is recommended as the first programming language for beginners
+
+x = 4
+
+# x is a variable name/objectname/identifier it is to stored 4 insdie of x
+#  4 is data/value/information
+
+# import keyword
+#
+# print(keyword.kwlist)
+
+
+
+
+# Applications of Python
+# We can use Python everywhere because Python is known for its general-purpose nature,
+# which makes it applicable in almost every domain of software development. The most common application areas are:
+#
+# Desktop Applications
+# Web Applications
+# Database Applications
+# Network Programming
+# Games
+# Data Analysis Applications
+# Machine Learning
+# Artificial Intelligence Applications
+# IoT
+
+# print('hello world')
+
+
+# Indentation
+# Python indentation tells a Python interpreter that the group of statements belongs to a particular block of code.
+# The indentation makes the code look neat, clean, and more readable.
+
+# space at the beging of code line
+
+# x1 = [1,2,3,4,5,1,2,3,4,5,1]
+# d1 ={}
+# for i in x1:
+#     if i in d1:
+#         d1[i]+=1
+#     else:
+#         d1[i]=1
+# print(d1)
+
+# print('hello python')
