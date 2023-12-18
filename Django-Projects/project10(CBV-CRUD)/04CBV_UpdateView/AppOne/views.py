c323d7a6d07d2fde919f354516cb9a1db5a1c136
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-12-18 14:59:31 +0530

    py

---
+++ Django-Projects/project10(CBV-CRUD)/04CBV_UpdateView/AppOne/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic import ListView,CreateView,DetailView,UpdateView
+from AppOne.models import Student
+# Create your views here.
+
+class StudentListView(ListView):
+    model =  Student
+#     By default_Template name is student_list.html
+#     By Default context_object_name is student_list
+
+class StudentDetailView(DetailView):
+    model = Student
+    #     By default_Template name is student_detail.html
+    #     By Default context_object_name is student
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    fields = '__all__'
+#     By default_Template name is student_form.html
+#     By Default context_object_name is student
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = '__all__'
+
+
+
+
